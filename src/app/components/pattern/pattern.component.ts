b89f6ac86e3949d571e8f062309c99f98ac608fa
Muhammed Doğancan Yılmazoğlu <muhammed.yilmazoglu@anywai.ch>
2023-05-06 17:26:03 +0300

    add rotation support

--- src/app/components/pattern/pattern.component.ts
+++ src/app/components/pattern/pattern.component.ts
@@ -19,7 +19,10 @@ export class PagePatternComponent implements OnInit {
   slitHeightPercentage: number = 0;
 
   @Input()
-  color: string = ''
+  color: string = '';
+
+  @Input()
+  rotation: string = '';
 
   pageHeight: number = 0;
   pageWidth: number = 0;
@@ -37,6 +40,27 @@ export class PagePatternComponent implements OnInit {
     } else {
       return (this.patternHeight * (100 - this.slitHeightPercentage)) / 100;
     }
+  }
+
+  get slitWidth() {
+    if (this.slitHeightPercentage > 100 || this.slitHeightPercentage < 0) {
+      return 0;
+    } else {
+      return (this.patternWidth * (100 - this.slitHeightPercentage)) / 100;
+    }
+  }
+
+  get patternRotation() {
+    if (
+      this.rotation === 'left' ||
+      this.rotation === 'right' ||
+      this.rotation === 'up' ||
+      this.rotation === 'down'
+    ) {
+      return this.rotation;
+    }
+
+    return 'up';
   }
 
   @HostListener('window:resize', ['$event'])
@@ -57,12 +81,62 @@ export class PagePatternComponent implements OnInit {
     this.pageWidth = window.innerWidth;
   }
 
-  patternPath() {
+  patternPathHandler() {
+    if (this.patternRotation === 'up') {
+      return this.upPatternPath();
+    }
+
+    if (this.patternRotation === 'down') {
+      return this.downPatternPath();
+    }
+
+    if (this.patternRotation === 'left') {
+      return this.leftPatternPath();
+    }
+
+    if (this.patternRotation === 'right') {
+      return this.rightPatternPath();
+    }
+
+    return this.upPatternPath();
+  }
+
+  upPatternPath() {
     return `M 0 0 
       L 0 ${this.patternHeight}
       L ${this.patternWidth}  ${this.patternHeight} 
       L  ${this.patternWidth} 0 
       L ${this.patternWidth / 2} ${this.slitHeight}
+      L 0 0
+      Z`;
+  }
+
+  downPatternPath() {
+    return `M 0 0 
+      L ${this.patternWidth} 0
+      L ${this.patternWidth}  ${this.patternHeight}
+      L ${this.patternWidth / 2} ${this.slitHeight} 
+      L  0 ${this.patternHeight} 
+      L 0 0
+      Z`;
+  }
+
+  leftPatternPath() {
+    return `M 0 0 
+      L ${this.patternWidth} 0
+      L ${this.patternWidth}  ${this.patternHeight}
+      L 0 ${this.patternHeight}
+      L ${this.slitWidth} ${this.patternHeight / 2}
+      L 0 0
+      Z`;
+  }
+
+  rightPatternPath() {
+    return `M 0 0 
+      L 0 ${this.patternHeight}
+      L ${this.patternWidth}  ${this.patternHeight}
+      L ${this.slitWidth} ${this.patternHeight / 2}
+      L ${this.patternWidth} 0
       L 0 0
       Z`;
   }
